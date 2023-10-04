69bc2f8375d079cb50dc2b5f45c47b3130bcd2b2
Benschar1 <tsvihbenschar@gmail.com>
2023-10-04 11:28:17 -0400

    remove fn main comment

--- src/main.rs
+++ src/main.rs
@@ -23,52 +23,6 @@ enum FileType {
 }
 
 fn main() -> Result<()> {
-    // let x = Node {
-    //     bare_node: BareNode::named("FuncDef"),
-    //     other: treehouse::node_types::types::NodeType::Internal {
-    //         fields: HashMap::from_iter(vec![
-    //             (
-    //                 "name".into(),
-    //                 Child {
-    //                     multiple: false,
-    //                     required: true,
-    //                     node_types: vec![BareNode::named("Ident")],
-    //                 },
-    //             ),
-    //             (
-    //                 "params".into(),
-    //                 Child {
-    //                     multiple: true,
-    //                     required: false,
-    //                     node_types: vec![
-    //                         BareNode::named("RegularParam"),
-    //                         BareNode::named("SelfParam"),
-    //                         BareNode::named("DefaultParam"),
-    //                         BareNode::named("VarArgParam"),
-    //                     ],
-    //                 },
-    //             ),
-    //             (
-    //                 "return_type".into(),
-    //                 Child {
-    //                     multiple: false,
-    //                     required: true,
-    //                     node_types: vec![BareNode::named("Type")],
-    //                 },
-    //             ),
-    //         ]),
-    //         children: Child {
-    //             multiple: false,
-    //             required: false,
-    //             node_types: vec![BareNode::named("Attr")],
-    //         },
-    //     },
-    // };
-
-    // let y = gen_type(&x);
-
-    // println!("\n\nFile:\n{:#?}\n\n{}", y, prettyplease::unparse(&y));
-
     let cli = Cli::parse();
     let file = File::open(cli.file)?;
     println!(
